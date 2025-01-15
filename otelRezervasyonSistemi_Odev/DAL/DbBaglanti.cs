2ac364872eaf05776e8254584ca708fc418e8b5b
gizemlaydemir <giz3maydemir@gmail.com>
2025-01-15 23:00:17 +0300

    projeAktar

---
+++ otelRezervasyonSistemi_Odev/DAL/DbBaglanti.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using MySql;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace otelRezervasyonSistemi_Odev.DAL
+{
+    public class DbBaglanti
+    {
+        public static MySqlConnection BaglantiCagır()
+        {
+            //aaaaaaaaa
+            try
+            {
+                MySqlConnection baglanti = new MySqlConnection("Server=172.21.54.253;Database=25_132330052;User=25_132330052;Password=!nif.ogr52AY;");
+                baglanti.Open();
+                return baglanti;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Bağlantıda bir hata oluştu: {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
---
+++ otelRezervasyonSistemi_Odev/DAL/MusteriDAL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace otelRezervasyonSistemi_Odev.DAL
+{
+    internal class MusteriDAL
+    {
+    }
+}
