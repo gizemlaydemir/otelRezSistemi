bab7cb46cb838972bea99458339c375a202476d8
gizemlaydemir <giz3maydemir@gmail.com>
2025-01-17 17:08:36 +0300

    degisiklik_17_01_25
    rezervasyon oluşturma formunu ekleme.

---
+++ otelRezervasyonSistemi_Odev/RezervasyonOlusturma.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace otelRezervasyonSistemi_Odev
+{
+    public partial class RezervasyonOlusturma : Form
+    {
+        public RezervasyonOlusturma()
+        {
+            InitializeComponent();
+        }
+    }
+}
